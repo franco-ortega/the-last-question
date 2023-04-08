f314607af83402d6cc708e04eda9ee017fd8c05c
Franco Ortega <francopdx@gmail.com>
2023-04-08 13:13:32 -0700

    add rainbow mode

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const body = document.querySelector('body')
-const button = document.querySelector('button');
+const body = document.querySelector('body');
+const dark = document.querySelector('#dark');
+const rainbow = document.querySelector('#rainbow');
 
-console.log(button.textContent);
+const modeEnum = {
+  dark: {
+    mode: 'dark-mode',
+    title: 'Dark Mode'
+  },
+  rainbow: {
+    mode: 'rainbow-mode',
+    title: 'Rainbow Mode'
+  },
+  light: {
+    title: 'Light Mode'
+  }
+}
 
-button.addEventListener('click', () => {
-  console.log('click');
-  body.classList.toggle('dark-mode');
+// if a "non-light mode" option is on, then the default textContent for that mode has been replaced with 'Light Mode'
+const isThisModeOn = (button) => {
+  return button.textContent === 'Light Mode';
+}
 
-  button.textContent = button.textContent === 'Dark Mode' ? 'Light Mode' : 'Dark Mode'
-})
+const turnOffMode = (button, mode) => {
+  body.classList.toggle(mode.mode)
+  button.textContent = mode.title;
+}
+
+dark.addEventListener('click', () => {
+  body.classList.toggle(modeEnum.dark.mode);
+  dark.textContent = dark.textContent === modeEnum.dark.title ? modeEnum.light.title : modeEnum.dark.title;
+
+  if(isThisModeOn(rainbow)) turnOffMode(rainbow, modeEnum.rainbow); 
+});
+
+rainbow.addEventListener('click', () => {
+  body.classList.toggle(modeEnum.rainbow.mode);
+  rainbow.textContent = rainbow.textContent === modeEnum.rainbow.title ? modeEnum.light.title : modeEnum.rainbow.title;
+
+  if(isThisModeOn(dark)) turnOffMode(dark, modeEnum.dark);
+});
